1bf9f7b544f5afc35d00f53f5b7852467b37155f
bum <kjb6558@naver.com>
2024-05-02 01:28:05 +0900

    메인 세미 마무리

--- resource/js/main.js
+++ resource/js/main.js
@@ -1,18 +1,6 @@
 $(document).ready(function() {
-
-    $(window).scroll(function() {
-        var scroll = $(window).scrollTop();
-        //console.log(scroll);
-        if (scroll >= 300) {
-            $(".sub02 header").addClass("change");
-        } else {
-            $(".sub02 header").removeClass("change");
-        }
-    });
-
-    $('.btn_menu').on('click',function(){
-        
-        $('.nav').toggleClass("show");
-    });
+    const el = document.querySelectorAll('.sticky-ui');
+    const observer = new IntersectionObserver( ([e]) => e.target.classList.toggle('stuck', e.intersectionRatio < 1),{ threshold: [1] });
+    el.forEach(element => observer.observe(element));
 });
 
