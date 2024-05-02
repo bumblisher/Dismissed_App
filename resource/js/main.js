ffb91550e45dc096b1e080386692ce932d37ca5a
bum <kjb6558@naver.com>
2024-05-03 00:30:25 +0900

    로그인 시작

--- resource/js/main.js
+++ resource/js/main.js
@@ -2,5 +2,6 @@ $(document).ready(function() {
     const el = document.querySelectorAll('.grp_tit.sticky');
     const observer = new IntersectionObserver( ([e]) => e.target.classList.toggle('stuck', e.intersectionRatio < 1),{ threshold: [1] });
     el.forEach(element => observer.observe(element));
+    
 });
 
