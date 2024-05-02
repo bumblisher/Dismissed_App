013f8f1317c3d48c2e74488b2109e0b542194837
bum <kjb6558@naver.com>
2024-05-02 23:44:30 +0900

    스티키 테스트

--- resource/js/main.js
+++ resource/js/main.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-    const el = document.querySelectorAll('.sticky-ui');
+    const el = document.querySelectorAll('.grp_tit.sticky');
     const observer = new IntersectionObserver( ([e]) => e.target.classList.toggle('stuck', e.intersectionRatio < 1),{ threshold: [1] });
     el.forEach(element => observer.observe(element));
 });
