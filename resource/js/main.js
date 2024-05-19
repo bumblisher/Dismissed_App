e647c5ff3b26a06de74d1c835a5684e1ffc670bd
bum <kjb6558@naver.com>
2024-05-19 22:26:56 +0900

    마무리 테스트

--- resource/js/main.js
+++ resource/js/main.js
@@ -1,7 +1,11 @@
 $(document).ready(function() {
-    const el = document.querySelectorAll('.grp_tit.sticky');
-    const observer = new IntersectionObserver( ([e]) => e.target.classList.toggle('stuck', e.intersectionRatio < 1),{ threshold: [1] });
-    el.forEach(element => observer.observe(element));
+    // const el = document.querySelectorAll('.grp_tit.sticky');
+    // const observer = new IntersectionObserver( ([e]) => e.target.classList.toggle('stuck', e.intersectionRatio < 1),{ threshold: [1] });
+    // el.forEach(element => observer.observe(element));
     
+    $(".list_acco .acco_tit").on("click", function(){
+        $(this).toggleClass("on");
+        $(this).siblings(".acco_cont").slideToggle(300);
+    });
 });
 
